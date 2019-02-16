dafff010c54263b83a47165168c47efa3bda8932
John Pavley <john@pavley.com>
2019-02-15 19:40:42 -0500

    Initial Commit

---
+++ Smith/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smith
+//
+//  Created by John Pavley on 2/15/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
