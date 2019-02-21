88e216738f3d32f07929cd7d923f7da8c1941dc9
John Pavley <john@pavley.com>
2019-02-21 17:46:21 -0500

    connected the cloud atlas view controller to the detail view controller

--- Smith/ViewController.swift
+++ Smith/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+
     }
 }
 
