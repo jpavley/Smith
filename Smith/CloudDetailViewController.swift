1ac4dd1afc6a4826a7e9fc18715eef996366b257
John Pavley <john@pavley.com>
2019-02-22 08:28:39 -0500

    CloudDetailViewController class

---
+++ Smith/CloudDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CloudDetailViewController.swift
+//  Smith
+//
+//  Created by John Pavley on 2/22/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import UIKit
+
+class CloudDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = false
+
+    }
+
+
+}
